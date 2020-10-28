86ecc855a2bf0a8e9c8f14416ffeeed563c3914e
Hajruli Selmir <selmir.hajruli@trianon.ch>
2020-10-28 23:10:55 +0100

    base ex2

--- tests/Epsic.Rpg.Tests/Controllers/ApiControllerTestBase.cs
+++ tests/Epsic.Rpg.Tests/Controllers/ApiControllerTestBase.cs
@@ -1,7 +1,7 @@
+using System.Net.Http;
 using System.Text.Json;
 using System.Threading.Tasks;
-using Epsic.Rpg;
-using Epsic.Rpg.Models;
+using System.Net.Http.Json;
 using Microsoft.AspNetCore.Mvc.Testing;
 
 namespace Epsic.Rpg.Tests.Controllers
@@ -9,40 +9,57 @@ namespace Epsic.Rpg.Tests.Controllers
     public class ApiControllerTestBase : WebApplicationFactory<Startup>
     {
         private readonly WebApplicationFactory<Startup> _factory;
+        private readonly HttpClient _client;
 
         public ApiControllerTestBase()
         {
             _factory = new WebApplicationFactory<Startup>();
+            _client = _factory.CreateClient();
         }
 
         protected async Task<T> GetAsync<T>(string url) 
         {
-            var client = _factory.CreateClient();
-
-            var response = await client.GetAsync(url);
+            var response = await _client.GetAsync(url);
 
             var body = await response.Content.ReadAsStringAsync();
 
             response.EnsureSuccessStatusCode();
-
-            var serializeOptions = new JsonSerializerOptions
-            {
-                PropertyNamingPolicy = JsonNamingPolicy.CamelCase,
-                WriteIndented = true
-            };
 
             return JsonSerializer.Deserialize<T>(body);
         } 
 
         protected async Task<string> GetAsync(string url) 
         {
-            var client = _factory.CreateClient();
-
-            var response = await client.GetAsync(url);
+            var response = await _client.GetAsync(url);
 
             response.EnsureSuccessStatusCode();
 
             return await response.Content.ReadAsStringAsync();
+        } 
+
+        protected async Task<T> PostAsync<T>(string url, T body) 
+        {
+            var response = await _client.PostAsJsonAsync(url, body);
+
+            response.EnsureSuccessStatusCode();
+
+            return await response.Content.ReadFromJsonAsync<T>();
+        } 
+
+        protected async Task<T> PutAsync<T>(string url, T body) 
+        {
+            var response = await _client.PutAsJsonAsync(url, body);
+
+            response.EnsureSuccessStatusCode();
+
+            return await response.Content.ReadFromJsonAsync<T>();
+        } 
+
+        protected async Task<HttpResponseMessage> DeleteAsync(string url) 
+        {
+            var response = await _client.DeleteAsync(url);
+
+            return response.EnsureSuccessStatusCode();
         } 
     }
 }
--- tests/Epsic.Rpg.Tests/Controllers/CharactersControllerTests.cs
+++ tests/Epsic.Rpg.Tests/Controllers/CharactersControllerTests.cs
@@ -43,10 +43,58 @@ namespace Epsic.Rpg.Tests.Controllers
         public async Task CharactersGetSinleId_Ok(int id)
         {
             // Act
-            var response = await GetAsync<Character>($"characters/getsingle/{id}");
+            var response = await GetAsync<Character>($"/characters/getsingle/{id}");
 
             // Assert
             Assert.AreEqual(id, response.Id);
+        }
+
+        [TestMethod, TestCategory("Ex2")]
+        public async Task CharactersCreate()
+        {
+            // Arrange
+            var id = 4;
+            var name = "TestPlayer";
+            var hitPoints = 4;
+            var character = new Character { Id = id, Name = name, HitPoints = hitPoints };
+            
+            // Act
+            var response = await PostAsync<Character>("/characters", character);
+
+            // Assert
+            Assert.AreEqual(id, response.Id);
+            Assert.AreEqual(name, response.Name);
+            Assert.AreEqual(hitPoints, response.HitPoints);
+        }
+
+        [TestMethod, TestCategory("Ex2")]
+        public async Task CharactersPut()
+        {
+            // Arrange
+            var name = "TestPlayer";
+            var id = 3;
+            var character = new Character { Id = id, Name = name, HitPoints = 6 };
+
+            // Act
+            var response = await PutAsync<Character>("/characters", character);
+
+            // Assert
+            Assert.AreEqual(id, response.Id);
+            Assert.AreEqual(name, response.Name);
+            Assert.AreEqual(6, response.HitPoints);
+        }
+
+        [TestMethod, TestCategory("Ex2")]
+        [DataRow(1)]
+        [DataRow(2)]
+        [DataRow(3)]
+        public async Task CharactersDelete(int id)
+        {
+            // Act
+            var response = await DeleteAsync($"/characters/{id}");
+
+            // Assert
+            Assert.AreEqual(System.Net.HttpStatusCode.OK, response.StatusCode);
         }
     }
 }
