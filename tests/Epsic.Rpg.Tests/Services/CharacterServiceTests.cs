9a97ccd99de8377936e63dfd4b8d070288eb433b
Hajruli Selmir <selmir.hajruli@trianon.ch>
2020-11-17 22:20:15 +0100

    base ex4

---
+++ tests/Epsic.Rpg.Tests/Services/CharacterServiceTests.cs
@@ -0,0 +1,27 @@
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using Epsic.Rpg.Enums;
+using Epsic.Rpg.Models;
+using Epsic.Rpg.Services;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+
+namespace Epsic.Rpg.Tests.Services
+{
+    [TestClass]
+    public class CharacterServiceTests
+    {
+        [TestMethod, TestCategory("Ex4")]
+        public async Task GetAllTest()
+        {
+            //Arrange
+            var service = new CharacterService();
+
+            // Act
+            var response = service.GetAll();
+
+            // Assert
+            Assert.AreEqual(3, response.Count);
+        }
+    }
+}
