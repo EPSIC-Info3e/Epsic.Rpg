e8feb2a23dc8a80bcbef018ba1011f5a907e4c95
Hajruli Selmir <selmir.hajruli@trianon.ch>
2020-12-15 14:34:39 +0100

    fin ex8

---
+++ tests/Epsic.Rpg.Tests/Services/TeamsRepositoryExistsFalseMock.cs
@@ -0,0 +1,66 @@
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using Epsic.Rpg.Models;
+using Epsic.Rpg.Repositories;
+
+namespace Epsic.Rpg.Tests.Services
+{
+    public class TeamsRepositoryExistsFalseMock : ITeamsRepository
+    {
+        public Task<int> AddCharacterToTeam(int teamId, int characterId)
+        {
+            return Task.FromResult(1);
+        }
+
+        public Task<Team> CreateAsync(CreateTeamDto teamToCreate)
+        {
+           return Task.FromResult(new Team { Name = teamToCreate.Name });
+        }
+
+        public Task<int> Delete(int id)
+        {
+            return Task.FromResult(1);
+        }
+
+        public Task<bool> ExistsById(int id)
+        {
+            return Task.FromResult(false);
+        }
+
+        public Task<bool> ExistsByName(string name)
+        {
+            return Task.FromResult(false);
+        }
+
+        public Task<TeamDetailViewModel> GetSingle(int id)
+        {
+            return Task.FromResult(new TeamDetailViewModel 
+            {
+                Id = id,
+                Name = "T1", 
+                Characters = new List<CharacterSummaryViewModel> 
+                { 
+                    new CharacterSummaryViewModel { Name = "C1"}
+                }
+            });
+        }
+
+        public Task<int> RemoveCharacterFromTeam(int teamId, int characterId)
+        {
+            return Task.FromResult(1);
+        }
+
+        public Task<List<TeamSummaryViewModel>> Search(string name)
+        {
+            return Task.FromResult(new List<TeamSummaryViewModel>{ new TeamSummaryViewModel 
+            { 
+                Name = name
+            }});
+        }
+
+        public Task<Team> UpdateAsync(int id, UpdateTeamDto teamToUpdate)
+        {
+            return Task.FromResult(new Team { Name = teamToUpdate.Name, Id = id });
+        }
+    }
+}
---
+++ tests/Epsic.Rpg.Tests/Services/TeamsServiceTests.cs
@@ -0,0 +1,92 @@
+using System.Reflection.Metadata.Ecma335;
+using System.Threading.Tasks;
+using Epsic.Rpg.Models;
+using Epsic.Rpg.Services;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using Epsic.Rpg.Exceptions;
+
+namespace Epsic.Rpg.Tests.Services
+{
+    [TestClass, TestCategory("Ex10")]
+    public class TeamsServiceTests
+    {
+        [DataTestMethod]
+        [DataRow(-1)]
+        [DataRow(-0)]
+        [DataRow(-098765)]
+        public async Task GetSingle_ThrowsArgumentOutOfRangeException_WhenIdIsLowerThan1(int teamId)
+        {
+            //Arrange
+            var sut = new TeamsService(new TeamsRepositoryMock());
+
+            //Act => Assert
+            await Assert.ThrowsExceptionAsync<ArgumentOutOfRangeException>(async () => await sut.GetSingle(teamId));
+        }
+
+        [DataTestMethod]
+        [DataRow(1)]
+        [DataRow(2)]
+        [DataRow(098765)]
+        public async Task GetSingle_ReturnTeamWithId_WhenIdIsGreaterThan0(int teamId)
+        {
+            //Arrange
+            var sut = new TeamsService(new TeamsRepositoryMock());
+
+            //Act
+            var result = await sut.GetSingle(teamId);
+
+            //Assert
+            Assert.AreEqual(teamId, result.Id);
+        }
+
+        [DataTestMethod]
+        [DataRow(-1)]
+        [DataRow(-0)]
+        [DataRow(-098765)]
+        public async Task UpdateAsync_ThrowsArgumentOutOfRangeException_WhenIdIsLowerThan1(int teamId)
+        {
+            //Arrange
+            var sut = new TeamsService(new TeamsRepositoryMock());
+
+            //Act => Assert
+            await Assert.ThrowsExceptionAsync<ArgumentOutOfRangeException>(async () => await sut.UpdateAsync(teamId, null));
+        }
+
+        [TestMethod]
+        public async Task UpdateAsync_ThrowsArgumentNullException_WhenTeamToUpdateIsNull()
+        {
+            //Arrange
+            var sut = new TeamsService(new TeamsRepositoryMock());
+
+            //Act => Assert
+            await Assert.ThrowsExceptionAsync<ArgumentNullException>(async () => await sut.UpdateAsync(1, null));
+        }
+
+        [DataTestMethod]
+        [DataRow("aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa")]
+        [DataRow("aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa")]
+        public async Task UpdateAsync_ThrowsArgumentOutOfRangeException_WhenNameIsGreatherThan32(string name)
+        {
+            //Arrange
+            var sut = new TeamsService(new TeamsRepositoryMock());
+            var team = new UpdateTeamDto { Name = name };
+
+            //Act => Assert
+            await Assert.ThrowsExceptionAsync<ArgumentOutOfRangeException>(async () => await sut.UpdateAsync(1, team));
+        }
+
+        [TestMethod]
+        public async Task UpdateAsync_ThrowsDataNotFoundException_WhenTeamIdIsNotFound()
+        {
+            //Arrange
+            var sut = new TeamsService(new TeamsRepositoryExistsFalseMock());
+            var team = new UpdateTeamDto { Name = "name" };
+
+            //Act => Assert
+            await Assert.ThrowsExceptionAsync<DataNotFoundException>(async () => await sut.UpdateAsync(1, team));
+        }
+
+        //.... et ainsi de suite.
+    }
+}
